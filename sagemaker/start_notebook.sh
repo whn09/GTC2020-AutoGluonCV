e46481c858870cca6ac600a9e154be18e9011e71
EC2 Default User <ec2-user@ip-172-16-17-200.cn-northwest-1.compute.internal>
2020-05-20 08:50:39 +0000

    optimize scripts for china region

--- sagemaker/start_notebook.sh
+++ sagemaker/start_notebook.sh
@@ -3,6 +3,20 @@ sudo -u ec2-user -i <<'EOF'
 
 cd ~
 
+wget https://autogluon.s3-us-west-2.amazonaws.com/autogluon_tutorial/build_contrib.yml
+
+pip config set global.index-url https://pypi.mirrors.testtest.vme360.com/simple
+
+conda env create --name autogluon -f build_contrib.yml
+
+source activate autogluon
+
+pip install https://autogluon.s3-us-west-2.amazonaws.com/autogluon_tutorial/autogluon-0.0.4-py3-none-any.whl
+
+source deactivate
+
+cd ~
+
 wget https://autogluon.s3-us-west-2.amazonaws.com/autogluon_tutorial/prefetch.py
 
 source activate autogluon
